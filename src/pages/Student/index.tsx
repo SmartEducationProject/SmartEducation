16d84a1fc59cc82012a7c61fff395061cbfe14df
MYY1106 <361598804@qq.com>
2022-07-24 23:07:32 +0800

    fix: 使用useRoutes统一管理路由

--- src/pages/Student/index.tsx
+++ src/pages/Student/index.tsx
@@ -1,16 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import router from 'router/student';
 
 const Students: FunctionComponent = () => {
-  return (
-    <Routes>
-      <Route>
-        {router.map((item) => (
-          <Route key={item.path} {...item} />
-        ))}
-      </Route>
-    </Routes>
-  );
+  return <div>index</div>;
 };
 export default Students;
