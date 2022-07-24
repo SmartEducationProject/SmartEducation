b6adb2459f30d1e6f103fe2203e2ab8e85dea64d
MYY1106 <361598804@qq.com>
2022-07-24 23:04:49 +0800

    refactor: 使用useRoutes统一管理路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
-import routers from 'router/app';
+import routes from 'router/app';
 import Header from '@/components/Header';
 import './app.less';
 import styles from './app.module.less';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useRoutes } from 'react-router-dom';
 
 function App() {
+  const elements = useRoutes(routes);
   return (
     <div className={styles['app']}>
       <Header />
-      <main className={styles['main']}>
-        <Routes>
-          {routers.map((item) => {
-            return <Route key={item.path} {...item}></Route>;
-          })}
-        </Routes>
-      </main>
+      <main className={styles['main']}>{elements}</main>
     </div>
   );
 }
--- src/router/app.tsx
+++ src/router/app.tsx
@@ -1,20 +1,41 @@
+import { Navigate, PathRouteProps, RouteObject } from 'react-router-dom';
+import Questionnaire from 'pages/Student/Questionnaire';
+import Welcome from 'pages/Student/Welcome';
 import Login from 'pages/Login';
 import Student from 'pages/Student';
 import Teacher from 'pages/Teacher';
-import { PathRouteProps } from 'react-router-dom';
 
-const routers: PathRouteProps[] = [
+const routes: RouteObject[] = [
   {
     path: '/',
     element: <Login />
   },
   {
     path: '/student/*',
-    element: <Student />
+    element: <Student />,
+    children: [
+      {
+        path: 'welcome',
+        element: <Welcome />
+      },
+      {
+        path: 'questionnaire',
+        element: <Questionnaire />
+      },
+      {
+        path: '*',
+        element: <Navigate to="/welcome" replace={true} />
+      }
+    ]
   },
   {
     path: '/teacher',
     element: <Teacher />
+  },
+  {
+    path: '*',
+    element: <Navigate to="/" replace={true} />
   }
 ];
-export default routers;
+
+export default routes;
