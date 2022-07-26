56a6af3f4cb16b243b250add317124cc006e2e7f
MYY1106 <361598804@qq.com>
2022-07-26 20:13:05 +0800

    feat: 大致完成Welcome页面

--- src/pages/Student/Welcome/index.tsx
+++ src/pages/Student/Welcome/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import styles from './index.module.less';
+import peopleImg from 'assets/pic/student/welcome-people.png';
 
 const Welcome = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <Link to="/student/questionnaire">Begin</Link>
+    <div className={styles['container']}>
+      <img src={peopleImg} className={styles['people-img']} />
+      <div className={styles['dialog-box']}>
+        <h2>叮咚~</h2>
+        <h2>你有一份调查报告需填写</h2>
+        <p>Tips: 填写后你将得知关于考研学校的一些数据，包括概率和排名，为保证其准确性，问卷需要根据事实填写哦~</p>
+        <button onClick={() => navigate('/student/questionnaire')} />
+      </div>
     </div>
   );
 };
