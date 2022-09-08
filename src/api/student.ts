e5fd256ddc2c2e3a393c677a1b8204b373d48037
MYY1106 <361598804@qq.com>
2022-09-08 15:36:15 +0800

    fix(student): change api

--- src/api/student.ts
+++ src/api/student.ts
@@ -1,8 +1,8 @@
+import useCqupt from '@/utils/useCqupt';
 import { axiosInstance } from 'utils/request';
 import processData from 'utils/processData';
 import { ILogin, IResponse } from 'types/Request';
 
-const sfrzh = JSON.parse(sessionStorage.getItem('studentInfo') as string)?.sfrzh;
 const token = JSON.parse(sessionStorage.getItem('studentInfo') as string)?.token;
 
 /**
@@ -27,31 +27,30 @@ export const submit = (data: any) => {
  * @description 获取同水平学生情况
  * @returns {Promise}
  */
-export const getSame = () => axiosInstance.get(`/student/same?sfrzh=${sfrzh}`).then((res) => res.data.data);
-// export const getSame = () => axiosInstance.get('/student/same').then((res) => res.data);
+export const getSame = () => axiosInstance.get('/student/same').then((res) => res.data.data);
 
 /**
  * @description 获取生活详情
  * @returns {Promise}
  */
-export const getDaily = () => axiosInstance.get(`/student/daily/test?sfrzh=${sfrzh}`).then((res) => res.data.data);
-// export const getDaily = () => axiosInstance.get('/student/daily').then((res) => res.data);
+export const getDaily = () => axiosInstance.get('/student/daily').then((res) => res.data.data);
 
 /**
  * @description 获取图书馆详情
  * @returns {Promise}
  */
-export const getLib = (): Promise<IResponse['data']> => axiosInstance.get(`/student/lib/test?sfrzh=${sfrzh}`).then((res) => res.data.data);
-// export const getLib = () => axiosInstance.get('/student/lib').then((res) => res.data);
+export const getLib = () => axiosInstance.get('/student/lib').then((res) => res.data.data);
 
 /**
  * @description 获取上岸概率详情
  * @returns {Promise}
  */
-export const getPredict = (): Promise<IResponse['data']> => axiosInstance.post(`/student/hasPredictTest?token=${token}`).then((res) => res.data.data);
+export const getPredict = (): Promise<IResponse['data']> => axiosInstance.post(`/student/hasPredict`).then((res) => res.data.data);
 
 /**
  * @description 获取比较详情
  * @returns {Promise}
  */
-export const getCompare = (): Promise<IResponse['data']> => axiosInstance.get(`/student/college/process/cy/test?token=${token}`).then((res) => res.data.data);
+export const getCompare = (isCqupt: boolean): Promise<IResponse['data']> => {
+  return axiosInstance.get(`/student/college/process/${isCqupt ? 'cy' : 'wx'}`).then((res) => res.data.data);
+};
--- src/pages/Student/College/index.tsx
+++ src/pages/Student/College/index.tsx
@@ -8,10 +8,12 @@ import StudyTime from './StudyTime';
 import { getCompare, getPredict } from 'api/student';
 import type { IPredict, IComparison } from 'types/college';
 import styles from './index.module.less';
+import useCqupt from '@/utils/useCqupt';
 
 const College = () => {
+  const isCqupt = useCqupt();
   const { data: predictData } = useQuery<IPredict>('predict', getPredict);
-  const { data: comparisonData } = useQuery<IComparison>('compare', getCompare);
+  const { data: comparisonData } = useQuery<IComparison>('compare', () => getCompare(isCqupt));
 
   return (
     <div className={styles['container']}>
