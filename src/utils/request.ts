91cfe98a0b937c213d0438480b6f4013be02f386
MYY1106 <361598804@qq.com>
2022-09-13 11:31:56 +0800

    fix: change sessionStorage to localStorage

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -14,7 +14,7 @@ axiosInstance.interceptors.request.use(
   (config: AxiosRequestConfig) => {
     if (config.url?.startsWith('/teacher/login') || config.url?.startsWith('/student/login')) return config; // 登录不需要带token
 
-    let token = sessionStorage.getItem('token') || JSON.parse(sessionStorage.getItem(`studentInfo`) as string).token;
+    let token = localStorage.getItem('token') || JSON.parse(localStorage.getItem(`studentInfo`) as string).token;
     if (token) config.headers!.token = token;
 
     return config;
