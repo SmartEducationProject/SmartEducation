d54b4c65a421fcb7124fc7b7cad7622ca4e2791d
MYY1106 <361598804@qq.com>
2023-06-28 22:05:04 +0800

    feat(/pages/student): add data to share page

--- src/pages/Student/Previous/Experience/index.tsx
+++ src/pages/Student/Previous/Experience/index.tsx
@@ -6,16 +6,18 @@ import PartHeader from 'components/PartHeader';
 import styles from './index.module.less';
 import { useExperience } from 'api/student';
 
-interface StudyExperienceProps {}
+interface StudyExperienceProps {
+  isTitleShow?: boolean;
+}
 
-const Experience: React.FC<StudyExperienceProps> = () => {
+const Experience: React.FC<StudyExperienceProps> = ({ isTitleShow = true }) => {
   const { data: cquptData } = useExperience(true);
   const { data: otherCollegeData } = useExperience(false);
   let index = 0; /** @description 控制背景的朝向 */
 
   return (
     <div className={styles['container']}>
-      <PartHeader title={'学长学姐经验'} icon={iconImg} />
+      {isTitleShow ? <PartHeader title={'学长学姐经验'} icon={iconImg} /> : ''}
       <main>
         {cquptData?.map((student) => (
           <ExperienceBox student={student} isRight={!!(index++ % 2)} key={student.name} />
--- src/pages/Student/Share/index.tsx
+++ src/pages/Student/Share/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './index.module.less';
 import png from '../../../assets/pic/student/college-study-experience.png';
+import Experience from '../Previous/Experience';
 
 export default function Share() {
   return (
@@ -10,17 +11,9 @@ export default function Share() {
         <img src={png} alt={'icon'} />
       </header>
 
-      <div className={styles['tips']}>
-        <div>
-          当前考研经验分享模块正在建设中，不过您可以在 <span>热门院校页面</span> 查看部分分享；
-        </div>
-        <div>
-          如果觉得这些分享有意义的话，希望您可以<span>关注本网站</span> ，在分享模块完善后，发表您的分享，帮助到更多的学弟学妹；
-        </div>
-        <div>感谢您的理解与参与！</div>
+      <div className={styles['box']}>
+        <Experience isTitleShow={false} />
       </div>
-
-      {/* <Outlet /> */}
     </>
   );
 }
