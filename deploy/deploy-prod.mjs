a9ee3cfb452ec4faaf3087f28799894dd398014d
MYY1106 <361598804@qq.com>
2024-09-23 21:48:21 +0800

    fix: fix deploy bug

--- deploy/deploy-prod.mjs
+++ deploy/deploy-prod.mjs
@@ -2,7 +2,7 @@ import deploy from "./deploy.mjs";
 import config from './config.mjs';
 
 deploy({
-    distDir: config.distDir + '/prod/dist', // dist文件夹在服务器的位置
+    distDir: config.distDir + '/prod', // dist文件夹在服务器的位置
     imageName: config.imageName + '-prod',
     containerName: config.containerName + '-prod',
     imageTag: '2.0',
